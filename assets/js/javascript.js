08e872254db76c84b4af10cd6f3ade2bb4e2f295
Chiranth Kothari <chiranthkothari96@gmail.com>
2017-01-03 19:07:00 +0530

    naw

---
+++ assets/js/javascript.js
@@ -0,0 +1,7 @@
+//flip mode simple
+//using data attributes
+$(".live-tile").liveTile();
+
+//without data-attributes
+//$("#tile1").liveTile({ mode:'flip', initDelay: 500 });
+//$("#tile2").last().liveTile({ mode:'flip' });
